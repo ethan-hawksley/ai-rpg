35e5bad8d7ed23aeef214220e746e8c6e92ddb80
ethan-hawksley <136483392+ethan-hawksley@users.noreply.github.com>
2025-06-27 17:01:55 +0100

    Add futuristic and unforgiving scenarios to start form; enhance form submission logging

--- src/routes/start/+page.svelte
+++ src/routes/start/+page.svelte
@@ -1,50 +1,68 @@
 <script lang="ts">
-    import { onMount} from "svelte";
+    import {onMount} from "svelte";
     import medieval from "$lib/assets/medieval.png";
-  import zombie from "$lib/assets/zombie.png";
+    import zombie from "$lib/assets/zombie.png";
+    import futuristic from "$lib/assets/futuristic.png"
+    import serious from "$lib/assets/serious.png";
+    import creative from "$lib/assets/creative.png";
+    import unforgiving from "$lib/assets/unforgiving.png"
 
-  import serious from "$lib/assets/serious.png";
-  import creative from "$lib/assets/creative.png";
-
-  onMount(() => {
-      const startForm = document.getElementById("start-form") as HTMLFormElement | null
-      if (startForm) {
-          startForm.addEventListener("submit", (e) => {
-              e.preventDefault();
-              const formData = new FormData(startForm)
-              const formProps = Object.fromEntries(formData)
-              console.log(formProps)
-          })
-      }
-  })
+    onMount(() => {
+        const startForm = document.getElementById("start-form") as HTMLFormElement | null
+        if (startForm) {
+            startForm.addEventListener("submit", (e) => {
+                e.preventDefault();
+                const formData = new FormData(startForm)
+                console.log(formData)
+                if (formData.has("scenario") && formData.has("storyteller")) {
+                    console.log(formData.get("scenario"), formData.get("storyteller"))
+                }
+            })
+        }
+    })
 </script>
 
 <h1>Start game</h1>
 <form id="start-form">
-    <h2>Pick your starting scenario.</h2>
-    <label class="radio">
-        <input id="medieval" name="scenario" type="radio" value="medieval"/>
-        <img alt="Medieval" src={medieval}>
-    </label>
+    <div>
+        <h2>Pick your starting scenario.</h2>
+        <label class="radio">
+            <input id="medieval" name="scenario" type="radio" value="medieval"/>
+            <img alt="Medieval" src={medieval}>
+        </label>
 
-    <label class="radio">
-        <input id="zombie" name="scenario" type="radio" value="zombie"/>
-        <img alt="Zombie" src={zombie}>
-    </label>
+        <label class="radio">
+            <input id="zombie" name="scenario" type="radio" value="zombie"/>
+            <img alt="Zombie" src={zombie}>
+        </label>
 
-    <h2>Pick your AI storyteller.</h2>
+        <label class="radio">
+            <input id="futuristic" name="scenario" type="radio" value="futuristic"/>
+            <img alt="Futuristic" src={futuristic}>
+        </label>
+    </div>
 
-    <label class="radio">
-        <input id="serious" name="storyteller" type="radio" value="serious"/>
-        <img alt="Serious" src={serious}>
-    </label>
+    <div>
+        <h2>Pick your AI storyteller.</h2>
 
-    <label class="radio">
-        <input id="creative" name="storyteller" type="radio" value="creative"/>
-        <img alt="Creative" src={creative}>
-    </label>
+        <label class="radio">
+            <input id="serious" name="storyteller" type="radio" value="serious"/>
+            <img alt="Serious" src={serious}>
+        </label>
+
+        <label class="radio">
+            <input id="creative" name="storyteller" type="radio" value="creative"/>
+            <img alt="Creative" src={creative}>
+        </label>
+
+        <label class="radio">
+            <input id="unforgiving" name="storyteller" type="radio" value="unforgiving"/>
+            <img alt="unforgiving" src={unforgiving}>
+        </label>
+    </div>
 
     <button type="submit">Start Game</button>
+
 </form>
 
 <style>
