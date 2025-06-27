8d97ab4662fd5e3d4a65cde6790828b3ad3e42c7
ethan-hawksley <136483392+ethan-hawksley@users.noreply.github.com>
2025-06-27 17:01:55 +0100

    Standardize codebase to use single quotes for strings in JS/TS files

--- src/routes/play/+page.ts
+++ src/routes/play/+page.ts
@@ -1,13 +1,13 @@
-import type {PageLoad} from "./$types";
+import type {PageLoad} from './$types';
 
 export const load: PageLoad = ({url}) => {
-    const param = url.searchParams.get("settings")
+    const param = url.searchParams.get('settings')
     let settings = {};
     if (param) {
         try {
             settings = JSON.parse(param);
         } catch {
-            settings = {storyteller: "serious", scenario: "medieval"}
+            settings = {storyteller: 'serious', scenario: 'medieval'}
         }
     }
     return {settings}
--- src/routes/play/data.js
+++ src/routes/play/data.js
@@ -1,13 +1,13 @@
-export const prompt = "You are an AI storyteller. Keep your responses to a medium length. Typically between 200 and 500 words, unless there is a reason otherwise.";
+export const prompt = 'You are an AI storyteller. Keep your responses to a medium length. Typically between 200 and 500 words, unless there is a reason otherwise.';
 
 export const storytellers = {
-  serious: "Act serious.",
-  creative: "Act creative.",
-  unforgiving: "Act unforgiving."
+  serious: 'Act serious.',
+  creative: 'Act creative.',
+  unforgiving: 'Act unforgiving.'
 };
 
 export const scenarios = {
-  medieval: "You stumble into an open yard. Knights and peasants mill around alike.",
-  zombie: "You quickly shut the door behind yourself, sealing the zombies on the other side.",
-  futuristic: "You stroll out of your open spaceship door. The towering sky city stretches out in front of you."
+  medieval: 'You stumble into an open yard. Knights and peasants mill around alike.',
+  zombie: 'You quickly shut the door behind yourself, sealing the zombies on the other side.',
+  futuristic: 'You stroll out of your open spaceship door. The towering sky city stretches out in front of you.'
 };
