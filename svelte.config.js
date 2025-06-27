db1105760ca11bff1ec68f81d13cf0aa4b236fbb
ethan-hawksley <136483392+ethan-hawksley@users.noreply.github.com>
2025-06-27 17:01:55 +0100

    Add fallback to index.html in SvelteKit config for improved SPA routing

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,8 @@ const config = {
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter(), paths: {
       base: process.env.NODE_ENV === "production" ? "/ai-rpg" : "",
-    }
+      fallback: 'index.html'
+    },
   }
 };
 
